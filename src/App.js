a547bf094abb81b5ec362533c95f5b331de353e6
Kyle Stephen Tancio <kylestancio@gmail.com>
2023-03-27 22:27:11 +0900

    Horizontal scroll with hover effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import './App.css'
 
 export default function App() {
+  
   return (
     <>
-      <h1 className='text-center'>Hello World</h1>
+      <h1 className='text-center'>Horizontal Sroll</h1>
+      <h3 className='ms-2'>Category 1</h3>
+      <div className='h-scroll-parent'>
+        <div className='h-scroll-child' style={{backgroundColor: 'red'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'orange'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'yellow'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'green'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'blue'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'indigo'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'violet'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'red'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'orange'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'yellow'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'green'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'blue'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'indigo'}}></div>
+        <div className='h-scroll-child' style={{backgroundColor: 'violet'}}></div>
+      </div>
     </>
   )
 }
